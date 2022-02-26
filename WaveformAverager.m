d42029f35b1fab1771787c3736f2aa58a1f06ae9
kevinsum <Kevinsum20051996@hotmail.com>
2022-02-26 17:38:46 +0000

    Output midi notes instead of note name

--- WaveformAverager.m
+++ WaveformAverager.m
@@ -14,45 +14,13 @@ for idx = 1 : length(audioFiles)
     audioSamples.fileName = baseFileName;
     [audioSamples.samples, audioSamples.Fs] = audioread(fullFileName);
     
-    % Get note details (octave and note name) from file name
-    noteDetails = split(audioSamples.fileName); % Split file name (with space as delimiter) to get note and octave. Note has be be first in file name. E.g. 'A# 2.wav'
-    noteDetails{2} = noteDetails{2}(1); % For octave part of file name, get rid of .wav extention
-    % Save note details to structure
-    audioSamples.note = noteDetails{1};
-    audioSamples.octave = str2double(noteDetails(2));
+    % Remove '.wav' from file name to get only the midi note
+    audioSamples.midiNote = erase(audioSamples.fileName, ".wav"); 
+    audioSamples.midiNote = str2double(audioSamples.midiNote);
     
-    % Get freq from note details (use equal temperature and A0 - 27.50Hz as
-    % starting note)
-    A0 = 27.50;
-    switch audioSamples.note
-        case 'C'
-            noteDiff = -9;
-        case 'C#'
-            noteDiff = -8;
-        case 'D'
-            noteDiff = -7;
-        case 'D#'
-            noteDiff = -6;
-        case 'E'
-            noteDiff = -5;
-        case 'F'
-            noteDiff = -4;
-        case 'F#'
-            noteDiff = -3;
-        case 'G'
-            noteDiff = -2;
-        case 'G#'
-            noteDiff = -1;
-        case 'A'
-            noteDiff = 0;
-        case 'A#'
-            noteDiff = 1;
-        case 'B'
-            noteDiff = 2;
-        otherwise
-            disp('Invalid note name')
-    end
-    audioSamples.noteFreq = A0*(2.^(((audioSamples.octave*12)+noteDiff)/12));
+    % Get freq from midi note  (use equal temperature i.e. A4 = 440 Hz)
+    A4 = 440;
+    audioSamples.noteFreq = A4*(2.^((audioSamples.midiNote - 69)/12));
     
     % Get number of samples in one cycle
     audioSamples.samplesPerCycle = audioSamples.Fs/audioSamples.noteFreq;
